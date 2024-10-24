35762b1d3782a63255876722b8782832a8f2f10c
LorenzoBellomo <lorenzo.bellomo@sns.it>
2024-10-24 17:40:38 +0200

    added modules

---
+++ README.md
@@ -0,0 +1,3 @@
+# SortedStaticBenchmark
+
+This repository contains the benchmark on Sorted Integers for standard, learned, and compressed indices. 
---
+++ install.sh
@@ -0,0 +1,24 @@
+
+# GOOGLE BENCHMARK
+echo "Installing Google Benchmark"
+cd benchmark
+cmake -E make_directory "build"
+cmake -E chdir "build" cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on -DCMAKE_BUILD_TYPE=Release ../
+cmake --build "build" --config Release 
+cd ..
+
+# for roaring (used through cmake)
+echo "Installing required tool for using Roaring through CMake"
+mkdir build 
+cd build 
+mkdir -p cmake
+wget -O cmake/CPM.cmake https://github.com/cpm-cmake/CPM.cmake/releases/latest/download/get_cpm.cmake
+cd ..
+
+# SDSL
+mkdir lib
+cd indices/sdsl-lite
+./install ../../lib
+cd ../..
+
+# TODO MANCA DS2I e roba per rmi
