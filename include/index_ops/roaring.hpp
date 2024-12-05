07381a7839ebbbc89924f7d26e721f82a4178f65
LorenzoBellomo <lorenzo.bellomo@sns.it>
2024-12-05 13:05:04 +0100

    added roaring

---
+++ include/index_ops/roaring.hpp
@@ -0,0 +1,61 @@
+#ifndef ROARINGINTERFACE_HPP
+#define ROARINGINTERFACE_HPP
+
+#include <iostream>
+#include <vector>
+#include <limits>
+#include "roaring.h"
+#include "roaring.hh"
+#include "roaring64map.hh"
+
+template <typename T>
+class RoaringInterface {
+private:
+    roaring::Roaring r32;
+    roaring::Roaring64Map r64;
+
+public:
+    void prepare(std::vector<T> data_) {}
+
+    void build(std::vector<T> data_) {
+        if constexpr(sizeof(T) == 4) {
+            for (auto x : data_) 
+                r32.add(x);
+            r32.runOptimize();
+        } else {
+            for (auto x : data_) 
+                r64.add(x);
+            r64.runOptimize();
+        }
+    }
+
+    T access(size_t idx) {
+        if constexpr(sizeof(T) == 4) {
+            auto iter = r32.begin();
+            std::advance(iter, idx);
+            return *iter;
+        }
+        auto iter = r64.begin();
+        std::advance(iter, idx);
+        return *iter;
+    }
+
+    T next_geq(T q) {
+        if constexpr(sizeof(T) == 4) {
+            auto iter = r32.begin();
+            iter.equalorlarger(q);
+            return *iter;
+        }
+        auto iter = r64.begin();
+        iter.move(q);
+        return *iter;
+    }
+
+    size_t size_in_bytes() {
+        if constexpr(sizeof(T) == 4) 
+            return r32.getSizeInBytes();
+        return r64.getSizeInBytes();
+    }
+};
+
+#endif // ROARINGINTERFACE_HPP
--- src/missing.cpp
+++ src/missing.cpp
@@ -7,6 +7,7 @@
 #include <index_ops/pgm.hpp>
 #include <index_ops/pgmpp.hpp>
 #include <index_ops/sdsl_eliasfano.hpp>
+#include <index_ops/roaring.hpp>
 
 template <class IDX, typename T>
 void Benchmark(benchmark::State& state, std::string dataset_name) {
@@ -27,9 +28,9 @@ void Benchmark(benchmark::State& state, std::string dataset_name) {
 }
 
 int main(int argc, char** argv) {
-    std::vector<std::string> datasets({"wiki_ts_200M_uint64", "books_200M_uint32", "lognormal_uint32", "companynet_uint32",
+    std::vector<std::string> datasets({"wiki_ts_200M_uint64", "lognormal_uint32", "fb_200M_uint64", "companynet_uint32",
     "normal_uint32", "wiki_ts_200M_uint32", "zipf_uint32", "books_800M_uint64", "exponential_uint32", "friendster_50M_uint32", 
-    "osm_cellids_800M_uint64", "fb_200M_uint64"});
+    "osm_cellids_800M_uint64", "books_200M_uint32"});
 
 #ifdef DEBUG
     auto num_iter = 1;
@@ -47,12 +48,14 @@ int main(int argc, char** argv) {
             benchmark::RegisterBenchmark(dataset+"_PGM128", Benchmark<PGMInterface<uint32_t, 128>, uint32_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM++8", Benchmark<PGMPPInterface<uint32_t, 8>, uint32_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM++32", Benchmark<PGMPPInterface<uint32_t, 32>, uint32_t>, dataset)->Iterations(num_iter);
-            benchmark::RegisterBenchmark(dataset+"_PGM++128", Benchmark<PGMPPInterface<uint32_t, 128>, uint32_t>, dataset)->Iterations(num_iter);*/
-            benchmark::RegisterBenchmark(dataset+"_SDSL-EliasFano", Benchmark<SDSLEliasFanoInterface<uint32_t>, uint32_t>, dataset)->Iterations(num_iter);
+            benchmark::RegisterBenchmark(dataset+"_PGM++128", Benchmark<PGMPPInterface<uint32_t, 128>, uint32_t>, dataset)->Iterations(num_iter);
+            benchmark::RegisterBenchmark(dataset+"_SDSL-EliasFano", Benchmark<SDSLEliasFanoInterface<uint32_t>, uint32_t>, dataset)->Iterations(num_iter);*/
+            benchmark::RegisterBenchmark(dataset+"_Roaring", Benchmark<RoaringInterface<uint32_t>, uint32_t>, dataset)->Iterations(num_iter);
         } else {
             //benchmark::RegisterBenchmark(dataset+"_std::vector", Benchmark<StdVectorInterface<uint64_t>, uint64_t>, dataset)->Iterations(num_iter);
-            benchmark::RegisterBenchmark(dataset+"_SDSL-EliasFano", Benchmark<SDSLEliasFanoInterface<uint64_t>, uint64_t>, dataset)->Iterations(num_iter);
-            /*benchmark::RegisterBenchmark(dataset+"_PGM8", Benchmark<PGMInterface<uint64_t, 8>, uint64_t>, dataset)->Iterations(num_iter);
+            benchmark::RegisterBenchmark(dataset+"_Roaring", Benchmark<RoaringInterface<uint64_t>, uint64_t>, dataset)->Iterations(num_iter);
+            /*benchmark::RegisterBenchmark(dataset+"_SDSL-EliasFano", Benchmark<SDSLEliasFanoInterface<uint64_t>, uint64_t>, dataset)->Iterations(num_iter);
+            benchmark::RegisterBenchmark(dataset+"_PGM8", Benchmark<PGMInterface<uint64_t, 8>, uint64_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM32", Benchmark<PGMInterface<uint64_t, 32>, uint64_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM128", Benchmark<PGMInterface<uint64_t, 128>, uint64_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM++8", Benchmark<PGMPPInterface<uint64_t, 8>, uint64_t>, dataset)->Iterations(num_iter);
