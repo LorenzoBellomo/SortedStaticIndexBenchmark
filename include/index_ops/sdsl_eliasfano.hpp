3784cab09d4b2afc87e8afa79e2e89ea9497d46f
LorenzoBellomo <lorenzo.bellomo@sns.it>
2024-12-05 11:42:58 +0100

    added eliasfano

---
+++ include/index_ops/sdsl_eliasfano.hpp
@@ -0,0 +1,43 @@
+#ifndef SDSLEF_HPP
+#define SDSLEF_HPP
+
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <sdsl/bit_vectors.hpp>
+#include <sdsl/vectors.hpp>
+
+template <typename T>
+class SDSLEliasFanoInterface {
+private:
+    sdsl::sd_vector<> ef;
+    sdsl::rank_support_sd<> ef_rank;
+    sdsl::select_support_sd<> ef_select;
+
+public:
+
+    void prepare(std::vector<T> data_) {}
+
+    void build(std::vector<T> data_) {
+        ef = sdsl::sd_vector(data_.begin(), data_.end());
+        ef_rank = sdsl::rank_support_sd<>(&ef);
+        ef_select = sdsl::select_support_sd<>(&ef);
+    }
+
+    T access(size_t idx) {
+        return ef_select(idx + 1);
+    }
+
+    T next_geq(T q) {
+        auto rank = ef_rank(q);
+        if (rank >= ef.size()) 
+            return std::numeric_limits<T>::max();
+        return access(rank);
+    }
+
+    size_t size_in_bytes() {
+        return sdsl::size_in_bytes(ef);
+    }
+};
+
+#endif // SDSLEF_HPP
--- src/missing.cpp
+++ src/missing.cpp
@@ -1,10 +1,12 @@
 #include <benchmark/benchmark.h>
 #include <iostream>
 #include <fstream>
+#include <util.hpp>
+
 #include <index_ops/std_vector.hpp>
 #include <index_ops/pgm.hpp>
 #include <index_ops/pgmpp.hpp>
-#include <util.hpp>
+#include <index_ops/sdsl_eliasfano.hpp>
 
 template <class IDX, typename T>
 void Benchmark(benchmark::State& state, std::string dataset_name) {
@@ -25,9 +27,9 @@ void Benchmark(benchmark::State& state, std::string dataset_name) {
 }
 
 int main(int argc, char** argv) {
-    std::vector<std::string> datasets({"fb_200M_uint64", "books_200M_uint32", "lognormal_uint32", "companynet_uint32",
+    std::vector<std::string> datasets({"wiki_ts_200M_uint64", "books_200M_uint32", "lognormal_uint32", "companynet_uint32",
     "normal_uint32", "wiki_ts_200M_uint32", "zipf_uint32", "books_800M_uint64", "exponential_uint32", "friendster_50M_uint32", 
-    "osm_cellids_800M_uint64", "wiki_ts_200M_uint64"});
+    "osm_cellids_800M_uint64", "fb_200M_uint64"});
 
 #ifdef DEBUG
     auto num_iter = 1;
@@ -39,21 +41,23 @@ int main(int argc, char** argv) {
     for (auto dataset : datasets) {
         // registering benchmarks for different datasets, and all the indexes present in index_ops
         if (dataset.back() == '2')  { // HERE REGISTERING ONLY THE ONES THAT WORK FOR 32 BIT
-            benchmark::RegisterBenchmark(dataset+"_std::vector", Benchmark<StdVectorInterface<uint32_t>, uint32_t>, dataset)->Iterations(num_iter);
+            /*benchmark::RegisterBenchmark(dataset+"_std::vector", Benchmark<StdVectorInterface<uint32_t>, uint32_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM8", Benchmark<PGMInterface<uint32_t, 8>, uint32_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM32", Benchmark<PGMInterface<uint32_t, 32>, uint32_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM128", Benchmark<PGMInterface<uint32_t, 128>, uint32_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM++8", Benchmark<PGMPPInterface<uint32_t, 8>, uint32_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM++32", Benchmark<PGMPPInterface<uint32_t, 32>, uint32_t>, dataset)->Iterations(num_iter);
-            benchmark::RegisterBenchmark(dataset+"_PGM++128", Benchmark<PGMPPInterface<uint32_t, 128>, uint32_t>, dataset)->Iterations(num_iter);
+            benchmark::RegisterBenchmark(dataset+"_PGM++128", Benchmark<PGMPPInterface<uint32_t, 128>, uint32_t>, dataset)->Iterations(num_iter);*/
+            benchmark::RegisterBenchmark(dataset+"_SDSL-EliasFano", Benchmark<SDSLEliasFanoInterface<uint32_t>, uint32_t>, dataset)->Iterations(num_iter);
         } else {
-            benchmark::RegisterBenchmark(dataset+"_std::vector", Benchmark<StdVectorInterface<uint64_t>, uint64_t>, dataset)->Iterations(num_iter);
-            benchmark::RegisterBenchmark(dataset+"_PGM8", Benchmark<PGMInterface<uint64_t, 8>, uint64_t>, dataset)->Iterations(num_iter);
+            //benchmark::RegisterBenchmark(dataset+"_std::vector", Benchmark<StdVectorInterface<uint64_t>, uint64_t>, dataset)->Iterations(num_iter);
+            benchmark::RegisterBenchmark(dataset+"_SDSL-EliasFano", Benchmark<SDSLEliasFanoInterface<uint64_t>, uint64_t>, dataset)->Iterations(num_iter);
+            /*benchmark::RegisterBenchmark(dataset+"_PGM8", Benchmark<PGMInterface<uint64_t, 8>, uint64_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM32", Benchmark<PGMInterface<uint64_t, 32>, uint64_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM128", Benchmark<PGMInterface<uint64_t, 128>, uint64_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM++8", Benchmark<PGMPPInterface<uint64_t, 8>, uint64_t>, dataset)->Iterations(num_iter);
             benchmark::RegisterBenchmark(dataset+"_PGM++32", Benchmark<PGMPPInterface<uint64_t, 32>, uint64_t>, dataset)->Iterations(num_iter);
-            benchmark::RegisterBenchmark(dataset+"_PGM++128", Benchmark<PGMPPInterface<uint64_t, 128>, uint64_t>, dataset)->Iterations(num_iter);
+            benchmark::RegisterBenchmark(dataset+"_PGM++128", Benchmark<PGMPPInterface<uint64_t, 128>, uint64_t>, dataset)->Iterations(num_iter);*/
         }
     }
     benchmark::Initialize(&argc, argv);
